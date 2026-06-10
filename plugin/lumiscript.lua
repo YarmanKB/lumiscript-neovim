84376b919ad7268ec18b731dafe48bffebb7404e
Herman Berdnikov <berdnikov1802@gmail.com>
2026-06-10 00:43:58 -0400

    Comply to nvim-treesitter

--- plugin/lumiscript.lua
+++ plugin/lumiscript.lua
@@ -1,16 +1,14 @@
-local ok, parsers = pcall(require, "nvim-treesitter.parsers")
-if not ok then
-  return
-end
-
-local parser_url = vim.g.lumiscript_treesitter_url
-  or vim.g.lumiscript_treesitter_path
-  or "https://github.com/YarmanKB/lumiscript-treesitter"
-
-parsers.get_parser_configs().lumiscript = {
-  install_info = {
-    url = parser_url,
-    files = { "src/parser.c" },
-  },
-  filetype = "lumiscript",
-}
+vim.api.nvim_create_autocmd("User", {
+	pattern = "TSUpdate",
+	callback = function()
+		local ok, parsers = pcall(require, "nvim-treesitter.parsers")
+		if not ok then
+			return
+		end
+		parsers.lumiscript = {
+			install_info = {
+				url = "https://github.com/YarmanKB/lumiscript-treesitter",
+			},
+		}
+	end,
+})
